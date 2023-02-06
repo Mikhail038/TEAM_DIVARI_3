59d9fcc12f88e5a0055b6debba90f060590da4dc
Elisey <58297256+Elisey-e@users.noreply.github.com>
2023-02-06 17:52:00 +0300

    Add files via upload

--- pst_notation.cpp
+++ pst_notation.cpp
@@ -1,14 +1,14 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
-void post_notation (int N);
+#include <string.h>
+#include "rpn.h"
 
 
 void post_notation (int N){
     int max_len = 100;
-    char c;
+    int curr_len = 0;
+    //char c;
 
     FILE *fp = fopen("constant_expressions.txt", "r");
     FILE *fw = fopen("postfix_notation.txt", "w");
@@ -27,15 +27,19 @@ void post_notation (int N){
         //while (c != EOF || c != '\n'){
         //    sp_all[i][j] = c;
         //    j++;
+        //    c = fgetc(fp);
         //}
+        sp_all[i] = (char*) calloc(max_len, sizeof(char));
+        fscanf(fp, "%s", sp_all[i]);
+        curr_len = strlen(sp_all[i]);
+        sp_all[i] = (char*) realloc(sp_all[i], curr_len);
 
-        fscanf(fp, "%s", &sp_all[i]);
 
 
         //c = '\0';
         //sp_all[i][j] = c;
         //sp_all[i] = (char*) realloc(sp_all[i], j + 1);
-        sp_all[i] = post_conversion(sp_all[i]);
+        sp_all[i] = post_conversation(sp_all[i]);
 
         fprintf(fw, "%s\0\n", sp_all[i]);
     }
